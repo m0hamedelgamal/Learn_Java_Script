33b58d60e88113d8fac399c93fa25db5033b8178
m0hamedelgamal <mohamed.ali.elgamal@gmail.com>
2023-08-03 19:34:48 +0300

    Remove comments

--- MyScripts/array.js
+++ MyScripts/array.js
@@ -21,15 +21,6 @@ let zz = myarray.pop();
 console.log(myarray);
 console.log(zz);
 console.log(typeof zz);
-
-// myarray.unshift("test01");
-// console.log(myarray);
-// console.log(myarray.length);
-
-// myarray.pop();
-// console.log(myarray);
-// console.log(myarray.length);
-
 myarray.shift();
 // console.log(myarray);
 // console.log(myarray.length);
